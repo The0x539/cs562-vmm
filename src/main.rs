596135f4579216bea320b374209221aaa5e9aaec
The0x539 <the0x539@gmail.com>
2022-03-26 14:46:21 -0500

    remove MMIO

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use kvm_bindings::kvm_userspace_memory_region;
 use kvm_ioctls::{Kvm, VcpuExit, VcpuFd, VmFd};
 use memmap2::MmapMut;
 
-const MEM_SIZE: usize = 0x4000;
-
 #[allow(dead_code)]
 pub struct VirtualMachine {
     kvm_fd: Kvm,
@@ -24,7 +22,7 @@ impl VirtualMachine {
 
         let mem_region = kvm_userspace_memory_region {
             slot: 0,
-            flags: kvm_bindings::KVM_MEM_LOG_DIRTY_PAGES,
+            flags: Default::default(),
             guest_phys_addr,
             memory_size: mem_size as u64,
             userspace_addr: guest_mem.as_mut_ptr() as u64,
@@ -57,18 +55,6 @@ impl VirtualMachine {
         match exit {
             VcpuExit::IoIn(addr, data) => println!("io in {addr:x} {data:02x?}"),
             VcpuExit::IoOut(addr, data) => println!("io out {addr:x} {data:02x?}"),
-
-            VcpuExit::MmioRead(addr, data) => println!("mmio read from {addr:x}: {data:02x?}"),
-
-            VcpuExit::MmioWrite(addr, data) => {
-                println!("mmio write to {addr:x}: {data:02x?}");
-
-                let dirty_log = self.vm_fd.get_dirty_log(0, MEM_SIZE)?;
-                let num_dirty_pages = dirty_log.iter().map(|x| x.count_ones()).sum::<u32>();
-
-                assert_eq!(num_dirty_pages, 1);
-            }
-
             VcpuExit::Hlt => return Ok(true),
             r => anyhow::bail!("Unexpected exit: {r:?}"),
         }
@@ -79,14 +65,13 @@ impl VirtualMachine {
 fn main() -> Result<()> {
     #[rustfmt::skip]
     let asm_code = [
-        0xba, 0xf8, 0x03,             // mov $0x3f8, %dx
-        0x00, 0xd8,                   // add %bl, %al
-        0x04, b'0',                   // add $'0', %al
-        0xee,                         // out %al, %dx
-        0xec,                         // in %dx, %al
-        0xc6, 0x06, 0x00, 0x80, 0x00, // movl $0, (0x8000); This generates a MMIO Write.
-        0x8a, 0x16, 0x00, 0x80,       // movl (0x8000), %dl; This generates a MMIO Read.
-        0xf4,                         // hlt
+        0xba, 0xf8, 0x03, // mov $0x3f8, %dx
+        0x00, 0xd8,       // add %bl, %al
+        0x04, b'0',       // add $'0', %al
+        0xee,             // out %al, (%dx)
+        0xb0, b'\n',      // mov $'\n', %al
+        0xee,             // out %al, (%dx)
+        0xf4,             // hlt
     ];
 
     let vm = VirtualMachine::new(0x4000, 0x1000, &asm_code)?;
