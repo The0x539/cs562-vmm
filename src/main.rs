5ebe470ff1d20620352bc6b7eb9b15eab13591a1
The0x539 <the0x539@gmail.com>
2022-03-26 13:31:03 -0500

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
 use kvm_bindings::kvm_userspace_memory_region;
-use kvm_ioctls::VcpuExit;
-use kvm_ioctls::{Kvm, VcpuFd};
+use kvm_ioctls::{Kvm, VcpuExit, VcpuFd, VmFd};
 use memmap2::MmapMut;
+
+const MEM_SIZE: usize = 0x4000;
 
 fn main() -> Result<()> {
     #[rustfmt::skip]
@@ -20,19 +21,17 @@ fn main() -> Result<()> {
     let kvm = Kvm::new()?;
     let vm = kvm.create_vm()?;
 
-    let mem_size = 0x4000;
     let guest_phys_addr = 0x1000;
-    let slot = 0;
 
-    let mut guest_mem = MmapMut::map_anon(mem_size)?;
+    let mut guest_mem = MmapMut::map_anon(MEM_SIZE)?;
     guest_mem[..asm_code.len()].copy_from_slice(&asm_code);
 
     let mem_region = kvm_userspace_memory_region {
-        slot,
+        slot: 0,
         flags: kvm_bindings::KVM_MEM_LOG_DIRTY_PAGES,
         guest_phys_addr,
-        memory_size: mem_size as u64,
-        userspace_addr: guest_mem.as_ptr() as u64,
+        memory_size: MEM_SIZE as u64,
+        userspace_addr: guest_mem.as_mut_ptr() as u64,
     };
     unsafe { vm.set_user_memory_region(mem_region)? };
 
@@ -41,29 +40,10 @@ fn main() -> Result<()> {
     setup_regs(&mut vcpu, guest_phys_addr)?;
 
     loop {
-        match vcpu.run().expect("run failed") {
-            VcpuExit::IoIn(addr, data) => {
-                println!("io in {addr:x} {data:02x?}");
-            }
-            VcpuExit::IoOut(addr, data) => {
-                println!("io out {addr:x} {data:02x?}");
-            }
-            VcpuExit::MmioRead(addr, data) => {
-                println!("mmio read from {addr:x}: {data:02x?}");
-            }
-            VcpuExit::MmioWrite(addr, data) => {
-                println!("mmio write to {addr:x}: {data:02x?}");
-
-                let num_dirty_pages = vm
-                    .get_dirty_log(slot, mem_size)?
-                    .into_iter()
-                    .map(u64::count_ones)
-                    .sum::<u32>();
-
-                assert_eq!(num_dirty_pages, 1);
-            }
-            VcpuExit::Hlt => break,
-            r => anyhow::bail!("Unexpected exit: {r:?}"),
+        let vm_exit = vcpu.run()?;
+        let done = handle_exit(&vm, vm_exit)?;
+        if done {
+            break;
         }
     }
 
@@ -84,4 +64,29 @@ fn setup_regs(vcpu: &VcpuFd, rip: u64) -> Result<()> {
     vcpu.set_regs(&regs)?;
 
     Ok(())
+}
+
+fn handle_exit(vm: &VmFd, exit: VcpuExit) -> Result<bool> {
+    match exit {
+        VcpuExit::IoIn(addr, data) => println!("io in {addr:x} {data:02x?}"),
+        VcpuExit::IoOut(addr, data) => println!("io out {addr:x} {data:02x?}"),
+
+        VcpuExit::MmioRead(addr, data) => println!("mmio read from {addr:x}: {data:02x?}"),
+
+        VcpuExit::MmioWrite(addr, data) => {
+            println!("mmio write to {addr:x}: {data:02x?}");
+
+            let num_dirty_pages = vm
+                .get_dirty_log(0, MEM_SIZE)?
+                .into_iter()
+                .map(u64::count_ones)
+                .sum::<u32>();
+
+            assert_eq!(num_dirty_pages, 1);
+        }
+
+        VcpuExit::Hlt => return Ok(true),
+        r => anyhow::bail!("Unexpected exit: {r:?}"),
+    }
+    Ok(false)
 }
